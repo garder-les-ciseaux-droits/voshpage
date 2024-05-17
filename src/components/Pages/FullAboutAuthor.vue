79abe128b4099795173ec2a144140c9cabfd0b1b
Evan Vosh <wahotondo@gmail.com>
2024-05-17 22:03:00 +0200

    third change

--- src/components/Pages/FullAboutAuthor.vue
+++ src/components/Pages/FullAboutAuthor.vue
@@ -14,8 +14,8 @@
               <p class="text-white font-serif italic md:text-base text-xs">Эван Вош</p>
             </div>
           </div>
-          <div class="w-[1100px] h-[950px] md:h-[700px] flex justify-start items-center space-x-4 min-w-0 max-w-full">
-            <div class="w-[1000px] h-full flex flex-col items-start text-black space-y-6 px-10 md:px-0 mx-2 md:mx-10">
+          <div class="w-[1100px] h-auto md:h-[700px] py-4 flex justify-start items-center space-x-4 min-w-0 max-w-full">
+            <div class="w-[1000px] h-full flex flex-col items-start text-black space-y-6 px-10 md:px-0 mx-2 py-4 md:mx-10">
               <h1 class="font-serif font-semibold text-base md:text-xl">Я просто создаю мир</h1>
               <p class="font-serif text-xs md:text-base">Писать - это, конечно, классно. Тем не менее, я скорее люблю создавать книгу, чем писать её. Звучит странно, но это так. Продумывать сюжет, арки, главы, сцены и героев - вот, что приносит мне большее удовольствие. Жанр лёгких романов, или ранобэ, меня привлёк своей простотой, но чем больше я читал, тем лучше осознавал, что дело не в способе строить предложения, количества диалогов и деталей. Этот жанр позволяет творить и делать это так, как сам считаешь нужным. Мы сами определяем границы дозволенного.</p>
               <h2 class="font-serif font-semibold text-base md:text-xl">Миссия</h2>
