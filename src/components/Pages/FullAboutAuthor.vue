ddceec94902343f0956131665f8110cb020be642
Evan Vosh <wahotondo@gmail.com>
2024-05-17 21:56:50 +0200

    second changes

--- src/components/Pages/FullAboutAuthor.vue
+++ src/components/Pages/FullAboutAuthor.vue
@@ -4,7 +4,7 @@
             <Navigation/>
             <img class="h-[10px] w-full" src="/src/assets/Images/Divider.png"/>
         </div>
-        <div class="w-full h-[1350px] bg-white  flex flex-col items-center space-y-10 min-w-0 max-w-full overflow-y-auto pb-10">
+        <div class="w-full h-[1350px] bg-white flex flex-col items-center space-y-10 min-w-0 max-w-full overflow-y-auto">
           <div class="w-[1100px] h-[150px] bg-gray-600 flex rounded-3xl justify-center md:justify-start items-center px-6 mt-0 md:mt-10 space-x-4 min-w-0 max-w-full">
             <div class="w-[80px] h-full flex items-center justify-center">
               <img class="w-14 h-14 md:w-20 md:h-20 rounded-3xl" src="/src/assets/Images/2im.png"/>
@@ -14,10 +14,8 @@
               <p class="text-white font-serif italic md:text-base text-xs">Эван Вош</p>
             </div>
           </div>
-          <div class="w-[1100px] h-[700px] md:h-[700px] flex justify-start items-center space-x-4 min-w-0 max-w-full">
-            <div class="w-[80px] h-full flex">
-            </div>
-            <div class="w-[1000px] h-full flex flex-col items-start text-black space-y-6 px-10 md:px-0">
+          <div class="w-[1100px] h-[950px] md:h-[700px] flex justify-start items-center space-x-4 min-w-0 max-w-full">
+            <div class="w-[1000px] h-full flex flex-col items-start text-black space-y-6 px-10 md:px-0 mx-2 md:mx-10">
               <h1 class="font-serif font-semibold text-base md:text-xl">Я просто создаю мир</h1>
               <p class="font-serif text-xs md:text-base">Писать - это, конечно, классно. Тем не менее, я скорее люблю создавать книгу, чем писать её. Звучит странно, но это так. Продумывать сюжет, арки, главы, сцены и героев - вот, что приносит мне большее удовольствие. Жанр лёгких романов, или ранобэ, меня привлёк своей простотой, но чем больше я читал, тем лучше осознавал, что дело не в способе строить предложения, количества диалогов и деталей. Этот жанр позволяет творить и делать это так, как сам считаешь нужным. Мы сами определяем границы дозволенного.</p>
               <h2 class="font-serif font-semibold text-base md:text-xl">Миссия</h2>
@@ -30,6 +28,7 @@
           </div>
 
         </div>
+
     </div>
 </template>
 
